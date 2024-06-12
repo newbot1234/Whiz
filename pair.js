0c06d7b936a4caf44c641f0d11827f256227fa66
Kingdavid3333 <163170618+Kingdavid3333@users.noreply.github.com>
2024-06-12 03:34:49 +0100

    Initial commit

---
+++ pair.js
@@ -0,0 +1,106 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+    default: France_King,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("maher-zubair-baileys");
+
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+        async function FLASH_MD_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/'+id)
+     try {
+            let Pair_Code_By_France_King = France_King({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: ["Chrome (Linux)", "", ""]
+             });
+             if(!Pair_Code_By_France_King.authState.creds.registered) {
+                await delay(1500);
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await Pair_Code_By_France_King.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            Pair_Code_By_France_King.ev.on('creds.update', saveCreds)
+            Pair_Code_By_France_King.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(5000);
+                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                await delay(800);
+               let b64data = Buffer.from(data).toString('base64');
+               let session = await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id, { text: '' + b64data });
+
+               let FLASH_MD_TEXT = `
+*𝙎𝙀𝙎𝙎𝙄𝙊𝙉 𝙄𝙎 𝙊𝙉𝙇𝙄𝙉𝙀*
+*AKVID MD *
+*𝙊𝙒𝙉𝙀𝙍 AKVID*
+____________________________________
+╔════◇
+║『 AKVID-MD 𝙄𝙎 𝙍𝙀𝘼𝘿𝙔 𝙏𝙊 𝘿𝙀𝙋𝙇𝙊𝙔』
+║ YOUR SESSION IS READY. COPY IT  
+║ AND HOST IT ON YOUR WEB.
+╚════════════════════╝
+╔═════◇
+║ 『••• OWNER INFO •••』
+║ ❒ 𝐎𝐰𝐧𝐞𝐫: _https://Wa.me/+2348150924887_
+
+║ ❒ 𝐑𝐞𝐩𝐨: _https://github.com/Kingdavid3333_
+
+║ ❒ 𝐖𝐚𝐆𝐫𝐨𝐮𝐩: _https://whatsapp.com/channel/0029VagFXfAATRSrUq8ROT1e_
+
+║ ❒ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: _https://whatsapp.com/channel/0029VagFXfAATRSrUq8ROT1e_
+
+║ 🤖🤖🤖
+╚════════════════════╝ 
+ *© AKVID MD*
+___________________________________
+
+Don't Forget To Give Star⭐ To My Repo_`
+ await Pair_Code_By_France_King.sendMessage(Pair_Code_By_France_King.user.id,{text:FLASH_MD_TEXT},{quoted:session})
+ 
+
+        await delay(100);
+        await Pair_Code_By_France_King.ws.close();
+        return await removeFile('./temp/'+id);
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    FLASH_MD_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/'+id);
+         if(!res.headersSent){
+            await res.send({code:"Service is Currently Unavailable"});
+         }
+        }
+    }
+    return await FLASH_MD_PAIR_CODE()
+});
+module.exports = router
---
+++ qr.js
@@ -0,0 +1,111 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const QRCode = require('qrcode');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+	default: France_King,
+	useMultiFileAuthState,
+	jidNormalizedUser,
+	Browsers,
+	delay,
+	makeInMemoryStore,
+} = require("@whiskeysockets/baileys");
+
+function removeFile(FilePath) {
+	if (!fs.existsSync(FilePath)) return false;
+	fs.rmSync(FilePath, {
+		recursive: true,
+		force: true
+	})
+};
+const {
+	readFile
+} = require("node:fs/promises")
+router.get('/', async (req, res) => {
+	const id = makeid();
+	async function FLASH_MD_QR_CODE() {
+		const {
+			state,
+			saveCreds
+		} = await useMultiFileAuthState('./temp/' + id)
+		try {
+			let Qr_Code_By_France_King = France_King({
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+
+			Qr_Code_By_France_King.ev.on('creds.update', saveCreds)
+			Qr_Code_By_France_Kingr.ev.on("connection.update", async (s) => {
+				const {
+					connection,
+					lastDisconnect,
+					qr
+				} = s;
+				if (qr) await res.end(await QRCode.toBuffer(qr));
+				if (connection == "open") {
+					await delay(5000);
+					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+					await delay(800);
+				   let b64data = Buffer.from(data).toString('base64');
+				   let session = await Qr_Code_By_France_King.sendMessage(Qr_Code_By_France_King.user.id, { text: 'AKVID;;;' + b64data });
+	
+				   let FLASH_MD_TEXT = `
+*𝙎𝙀𝙎𝙎𝙄𝙊𝙉 𝙄𝙎 𝙊𝙉𝙇𝙄𝙉𝙀*
+*AKVID-MD*
+*𝙊𝙒𝙉𝙀𝙍 AKVID*
+____________________________________
+╔════◇
+║『 AKVID-MD 𝙄𝙎 𝙍𝙀𝘼𝘿𝙔 𝙏𝙊 𝘿𝙀𝙋𝙇𝙊𝙔』
+║ YOUR SESSION IS READY. COPY IT  
+║ AND HOST IT ON YOUR WEB.
+╚════════════════════╝
+╔═════◇
+║ 『••• OWNER INFO •••』
+║ ❒ 𝐎𝐰𝐧𝐞𝐫: _https://Wa.me/+2348150924887_
+
+║ ❒ 𝐑𝐞𝐩𝐨: _https://github.com/Kingdavid3333_
+
+║ ❒ 𝐖𝐚𝐆𝐫𝐨𝐮𝐩: _https://whatsapp.com/channel/0029VagFXfAATRSrUq8ROT1e_
+
+║ ❒ 𝐖𝐚𝐂𝐡𝐚𝐧𝐧𝐞𝐥: _https://whatsapp.com/channel/0029VagFXfAATRSrUq8ROT1e_
+
+║ 🤖🤖🤖
+╚════════════════════╝ 
+ *© AKVID MD*
+___________________________________
+
+_Don't Forget To Give Star⭐ To My Repo_`
+	 await Qr_Code_By_France_King.sendMessage(Qr_Code_By_France_King.user.id,{text:FLASH_MD_TEXT},{quoted:session})
+
+
+
+					await delay(100);
+					await Qr_Code_By_France_King.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					FLASH_MD_QR_CODE();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service is Currently Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await FLASH_MD_QR_CODE()
+});
+module.exports = router
